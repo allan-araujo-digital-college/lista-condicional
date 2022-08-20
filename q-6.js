dec2e21fecf9397415c0884523621abd072ca238
Allan Araújo Pinheiro <apinheiro@railz.ai>
2022-08-20 13:57:40 -0300

    Exercicio 6

---
+++ q-6.js
@@ -0,0 +1,8 @@
+// let a = Boolean(prompt('valor boolean a'))
+// let b = Boolean(prompt('valor boolean b'))
+let a = false;
+let b = false;
+
+if ((a && b) || (!a && !b)) {
+  console.log("determinado");
+}
